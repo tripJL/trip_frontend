d3aed8f38ec86429d8f32857dc1a5bed96bfcc18
leesh235 <leeshgh235@gmail.com>
2022-05-07 19:50:12 +0900

    add async util

--- src/modules/reducer/login.js
+++ src/modules/reducer/login.js
@@ -1,20 +1,22 @@
 import { LOGIN, LOGIN_SUCCESS, LOGIN_ERROR } from "../action/login";
+import { handleAsyncReducer } from "../../utils/reducerUtils";
 
 const initialState = {
     login: null,
 };
 
 const reducer = (state = initialState, action) => {
-    const { type, data } = action;
-    switch (type) {
-        case LOGIN:
-            return {
-                ...state,
-                login: data,
-            };
-        default:
-            return state;
-    }
+    // const { type, data } = action;
+    // switch (type) {
+    //     case LOGIN:
+    //         return {
+    //             ...state,
+    //             login: data,
+    //         };
+    //     default:
+    //         return state;
+    // }
+    return handleAsyncReducer(LOGIN, "login", true)(state, action);
 };
 
 export default reducer;
