772ab35c7bdc3d8b9a1a64b9aeb116adcbc6a96b
leesh235 <leeshgh235@gmail.com>
2022-05-07 19:49:37 +0900

    add redux and saga function

---
+++ src/utils/reducerUtils.js
@@ -0,0 +1,56 @@
+export const reducerUtils = {
+    initial: (inintialData) => ({
+        loading: false,
+        data: inintialData,
+        error: null,
+    }),
+    loading: (prevState) => ({
+        loading: false,
+        data: prevState,
+        error: null,
+    }),
+    success: (data) => ({
+        loading: true,
+        data,
+        error: null,
+    }),
+    error: (error) => ({
+        loading: true,
+        data: null,
+        error,
+    }),
+};
+
+//reducer module
+//사용하면 combineReducers에서 error발생
+export const handleAsyncReducer = (type, key, keepData = false) => {
+    const [SUCCESS, ERROR] = [`${type}_SUCCESS`, `${type}_ERROR`];
+    return (state, action) => {
+        switch (action.type) {
+            // case type:
+            //     return {
+            //         ...state,
+            //         [key]: reducerUtils.loading(
+            //             keepData ? state[key].data : null
+            //         ),
+            //     };
+            // case SUCCESS:
+            //     return {
+            //         ...state,
+            //         [key]: reducerUtils.success(action.data),
+            //     };
+            // case ERROR:
+            //     return {
+            //         ...state,
+            //         [key]: reducerUtils.error(action.data),
+            //     };
+            case type:
+                return {
+                    ...state,
+                    [key]: action.data,
+                };
+            default:
+                return state;
+        }
+    };
+};
